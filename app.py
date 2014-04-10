aee5e42063a78bb2cf944a39a67bfbb68fdd13f1
Sameer Gadkaree <gadkaree@gmail.com>
2014-04-10 13:28:16 -0500

    flask-hello-world-dynamic

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ from flask import Flask
 
 app=Flask(__name__)
 
+# add error handling
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
 
-def hello_world():
-	return "Hello, world!"
+# dynamic route
 
+@app.route("/test")
+@app.route("/test/<search_query>")
+
+
+
+def hello_world():
+	return "Hello, world???!!!"
+
+def search(search_query):
+	return search_query
+
+def search():
+	return "Hello"
+	
 if __name__=="__main__":
 	app.run()
