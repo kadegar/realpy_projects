2b663dc17c46a854760ef424c78e126111d53c86
Sameer Gadkaree <gadkaree@gmail.com>
2014-04-10 22:14:00 -0500

    added data to table

---
+++ sql/sqlb.py
@@ -0,0 +1,15 @@
+# Learning INSERT
+
+import sqlite3
+
+conn=sqlite3.connect("new.db")
+
+cursor=conn.cursor()
+
+cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+cursor.execute("INSERT INTO population VALUES('Chicago', 'IL', 3000000)")
+
+conn.commit()
+
+conn.close()
