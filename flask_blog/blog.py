c383b554624f97135bc449f04a130937a419fc55
Sameer Gadkaree <gadkaree@gmail.com>
2014-04-15 22:48:48 -0500

    Added post functionality

--- flask_blog/blog.py
+++ flask_blog/blog.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
-DATABASE='fblog.db'
+DATABASE='blog.db'
 USERNAME='admin'
 PASSWORD='admin'
 SECRET_KEY='topsecretpopsecret'
@@ -12,6 +13,16 @@ app.config.from_object(__name__)
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args,**kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -24,15 +35,23 @@ def login():
 			return redirect(url_for('main'))
 	return render_template('login.html', error=error)
 
+
 @app.route('/main')
+@login_required
 def main():
-	return render_template('main.html')
+	g.db=connect_db()
+	cur=g.db.execute('select * from posts')
+	posts=[dict(title=row[0],post=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
 
 @app.route('/logout')
 def logout():
 	session.pop('logged_in',None)
 	flash('You were logged out')
 	return redirect(url_for('login'))
+
+
 
 if __name__== '__main__':
 	app.run(debug=True)
