53af27488f800e87b3ab467aaf7578bbd271016d
Sameer Gadkaree <gadkaree@gmail.com>
2014-04-15 21:16:39 -0500

    added login function

--- flask_blog/blog.py
+++ flask_blog/blog.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
 
-DATABASE='blog.db'
+DATABASE='fblog.db'
+USERNAME='admin'
+PASSWORD='admin'
+SECRET_KEY='topsecretpopsecret'
 
 app=Flask(__name__)
 
@@ -10,7 +13,26 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-if __name__=='__main__':
-	app.run(debug=True)
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	error=None
+	if request.method=='POST':
+		if request.form['username']!=app.config['USERNAME'] or request.form['password']!=app.config['PASSWORD']:
+			error='Invalid credentials.  Please try again.'
+		else:
+			session['logged_in']=True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error)
 
-	
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in',None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
+
+if __name__== '__main__':
+	app.run(debug=True)
